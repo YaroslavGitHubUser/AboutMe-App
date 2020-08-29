de6806bbdbacc56328db73c7ffd7946e93c06757
Yaroslav <yaroslav.olex@gmail.com>
2020-08-29 11:19:39 +0300

    Initial Commit

---
+++ AboutMe App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe App
+//
+//  Created by Yaroslav on 29.08.2020.
+//  Copyright © 2020 Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
